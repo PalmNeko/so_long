4d5d4f2ab177acb193e5c5f3a87afb08692b7236
tookuyam <tookuyam@student.42.tokyo.jp>
2024-06-07 18:57:59 +0900

    update: free way

--- submit/mxw_new_window.c
+++ submit/mxw_new_window.c
@@ -6,28 +6,33 @@
 /*   By: tookuyam <tookuyam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 17:11:11 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/06/07 18:38:59 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/06/07 18:57:22 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
 #include <stdlib.h>
 #include "mxw.h"
+#include "libft.h"
 
 t_mxw_window	*mxw_new_window(t_mxw *mxw, int size_x, int size_y, char *title)
 {
 	t_mxw_window	*window;
+	t_free_args		frees[5];
 
 	window = (t_mxw_window *)malloc(sizeof(t_mxw_window));
 	if (window == NULL)
 		return (NULL);
+	frees[0] = (t_free_args){.free_func = free, .arg = window};
 	window->mxw = mxw;
 	window->img_buf = mxw_new_image(mxw, size_x, size_y);
 	if (window->img_buf == NULL)
-		return (free(window), NULL);
+		return (ft_free_iter(frees, 1), NULL);
+	frees[1] = (t_free_args){.free_func = mxw_destroy_image,
+		.arg = window->img_buf};
 	window->mlx_win = mlx_new_window(mxw->mlx, size_x, size_y, title);
 	if (window->mlx_win == NULL)
-		return (free(window), mxw_destroy_image(window->img_buf), NULL);
+		return (ft_free_iter(frees, 2), NULL);
 	window->title = title;
 	return (window);
 }
