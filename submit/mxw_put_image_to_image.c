cbce5868787be0657112f2a3ebf5929689d1d16e
tookuyam <tookuyam@student.42.tokyo.jp>
2024-06-07 15:32:07 +0900

    fix: mxw_put_image_to_image

--- submit/mxw_put_image_to_image.c
+++ submit/mxw_put_image_to_image.c
@@ -6,31 +6,35 @@
 /*   By: tookuyam <tookuyam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 20:09:32 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/06/06 16:17:19 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/06/07 15:28:10 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mxw.h"
-#include <stdio.h>
+#include <stddef.h>
+
+static int	__copy_pixel(t_mxw_image *put_image, t_mxw_fip_param *args);
 
 int	mxw_put_image_to_image(t_mxw_image *canvas, t_mxw_image *image,
 		int x, int y)
 {
-	int	index_x;
-	int	index_y;
+	mxw_foreach_image((t_mxw_foreach_image_param []){{
+		.image = canvas,
+		.f = __copy_pixel,
+		.width = image->width,
+		.height = image->height,
+		.offset_x = x,
+		.offset_y = y,
+		.result = NULL,
+		.your_param = image}});
+	return (0);
+}
+
+static int	__copy_pixel(t_mxw_image *put_image, t_mxw_fip_param *args)
+{
 	int	color;
 
-	index_y = 0;
-	while (index_y < image->height)
-	{
-		index_x = 0;
-		while (index_x < image->width)
-		{
-			color = mxw_pixel_get_from_image(image, index_x, index_y);
-			mxw_pixel_put_to_image(canvas, x + index_x, y + index_y, color);
-			index_x++;
-		}
-		index_y++;
-	}
+	color = mxw_pixel_get_from_image(put_image, args->image_x, args->image_y);
+	mxw_pixel_put_to_image(args->image, args->index_x, args->index_y, color);
 	return (0);
 }
