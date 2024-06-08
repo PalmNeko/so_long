f43dd69216c05a55f8e455ecc318f406a82f69cf
tookuyam <tookuyam@student.42.tokyo.jp>
2024-06-08 13:10:30 +0900

    fix: mxw_int_clean_window

--- submit/mxw_int_clean_windows.c
+++ submit/mxw_int_clean_windows.c
@@ -6,7 +6,7 @@
 /*   By: tookuyam <tookuyam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 23:21:32 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/06/07 23:27:03 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/06/08 13:00:00 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	mxw_int_clean_windows(t_mxw *mxw)
 {
+	if (mxw == NULL)
+		return;
 	while (mxw->window_list != NULL)
 		mxw_destroy_window(mxw->window_list->content);
 	return ;
