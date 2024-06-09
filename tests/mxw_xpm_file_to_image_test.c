745b5f7969838c38a052943ed9e6a397fb985740
tookuyam <tookuyam@student.42.tokyo.jp>
2024-06-09 17:05:24 +0900

    fix: compile error

--- tests/mxw_xpm_file_to_image_test.c
+++ tests/mxw_xpm_file_to_image_test.c
@@ -6,7 +6,7 @@
 /*   By: tookuyam <tookuyam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 15:15:41 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/06/09 12:41:00 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/06/09 17:01:41 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,21 @@ int	inner_test_setup(t_test_this *this)
 	t_mxw_image	*image;
 	image = mxw_xpm_file_to_image(this->mxw, "assets/textures.xpm");
 	if (image == NULL)
-		return (NULL);
+		return (-1);
 	mxw_put_image_to_window(this->win, image, 0, 0);
 	mxw_destroy_image(image);
 	mxw_flip_screen(this->win);
-	mxw_set_end(this->mxw);
 	return (0);
 }
 
 int	inner_test_loop(t_test_this *this)
 {
+	(void)this;
 	return (0);
 }
 
 int	inner_test_destroy(t_test_this *this)
 {
+	(void)this;
 	return (0);
 }
