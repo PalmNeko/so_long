5c97258e5aac9cf279198cf37d3a5b28df5a0f16
tookuyam <tookuyam@student.42tokyo.jp>
2024-06-11 03:10:17 +0900

    add: arrow key

--- submit/sl_keyboard_handler.c
+++ submit/sl_keyboard_handler.c
@@ -6,7 +6,7 @@
 /*   By: tookuyam <tookuyam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 18:15:04 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/06/10 18:10:20 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/06/11 03:04:20 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ int	sl_keyboard_handler(int keycode, t_sl_this *vars)
 		mxw_set_end(vars->mxw);
 	if (keycode == XK_Return)
 		mxw_flip_flipbbook(*vars->player->now_flipbook, 1);
-	if (keycode == XK_a)
+	if (keycode == XK_a || keycode == XK_Left)
 		sl_move_player(vars, vars->player->x - 1, vars->player->y);
-	if (keycode == XK_w)
+	if (keycode == XK_w || keycode == XK_Up)
 		sl_move_player(vars, vars->player->x, vars->player->y - 1);
-	if (keycode == XK_d)
+	if (keycode == XK_d || keycode == XK_Right)
 		sl_move_player(vars, vars->player->x + 1, vars->player->y);
-	if (keycode == XK_s)
+	if (keycode == XK_s || keycode == XK_Down)
 		sl_move_player(vars, vars->player->x, vars->player->y + 1);
 	return (0);
 }
