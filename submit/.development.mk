33ced70d22acb0ebc7b222dc801c9e7269fbd7f4
root <root@hp02>
2024-06-10 17:23:27 +0900

    add: check syntax

--- submit/.development.mk
+++ submit/.development.mk
@@ -3,6 +3,7 @@ watch-build:
 	@bash .watch-build.sh '-not -name "*.o" -not -name "*.d" -not -name "$(NAME)"'
 
 lint:
+	$(CC) $(SRCS) $(CFLAGS) -fsyntax-only $(SRCS)
 	@! norminette | grep -v -E ': OK!$$'
 
 watch-lint:
