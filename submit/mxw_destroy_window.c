3598b2ceab4b826f586581778f03c6035765f7ad
Tomoya Okuyama <nitunamoni13579@gmail.com>
2024-06-11 23:22:12 +0900

    fix: ft_voidnop type

--- submit/mxw_destroy_window.c
+++ submit/mxw_destroy_window.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mxw_destroy_window.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tookuyam <tookuyam@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tookuyam <tookuyam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 18:25:25 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/06/10 17:00:47 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/06/11 23:19:38 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,12 @@
 #include "mxw.h"
 #include "libft.h"
 
-void	ft_voidnop()
-{
-	return ;
-}
-
 void	mxw_destroy_window(t_mxw_window *win)
 {
 	if (win == NULL)
 		return ;
-	ft_lstdel_content(&win->mxw->window_list, win, ft_voidnop);
+	ft_lstdel_content(&win->mxw->window_list, win,
+		(void (*)(void *))ft_voidnop);
 	mxw_destroy_image(win->img_buf);
 	if (win->has_mlx_win == true)
 		mlx_destroy_window(win->mxw->mlx, win->mlx_win);
