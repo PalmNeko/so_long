fd929b673a1c20fcd4d13eb9da508f3b02094325
PalmNeko <nitunamoni13579@gmail.com>
2024-06-16 00:51:02 +0900

    improve: speed

--- submit/mxw_calc_alpha_blend.c
+++ submit/mxw_calc_alpha_blend.c
@@ -6,39 +6,37 @@
 /*   By: tookuyam <tookuyam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 17:54:45 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/06/09 18:20:55 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/06/16 00:50:50 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mxw.h"
 
-int _calc_alpha_blend(int base_color, int overlap_color, int (*f)());
-
-int	mxw_calc_alpha_blend(int base_color, int overlap_color)
-{
-	int new_r;
-	int new_g;
-	int new_b;
-	int	new_t;
-
-	new_r = _calc_alpha_blend(base_color, overlap_color, mxw_get_color_r);
-	new_g = _calc_alpha_blend(base_color, overlap_color, mxw_get_color_g);
-	new_b = _calc_alpha_blend(base_color, overlap_color, mxw_get_color_b);
-	new_t = 0;
-	return (mxw_create_trgb(new_t, new_r, new_g, new_b));
-}
-
-int _calc_alpha_blend(int base_color, int overlap_color, int (*f)())
+/**
+ * @brief alpha blend
+ * @note this function many is called. I write for speed.
+ */
+int	mxw_calc_alpha_blend(int base_color, int fore_color)
 {
 	double	alpha;
 	int	base_alpha;
-	int overlap_alpha;
+	int fore_alpha;
+	int	fore_t;
 
+	fore_t = mxw_get_color_t(fore_color);
+	if (fore_t == 0)
+		return (fore_color);
+	else if (fore_t == 255)
+		return (base_color);
 	base_alpha = (255 - mxw_get_color_t(base_color));
-	overlap_alpha = (255 - mxw_get_color_t(overlap_color));
-	if (base_alpha == 0)
-		alpha = 1;
-	else
-		alpha = overlap_alpha / base_alpha;
-	return (f(base_color) * (1 - alpha) + f(overlap_color) * alpha);
+	fore_alpha = (255 - mxw_get_color_t(fore_color));
+	alpha = fore_alpha / base_alpha;
+	return (mxw_create_trgb(
+		0,
+		mxw_get_color_r(base_color) * (1 - alpha)
+			+ mxw_get_color_r(fore_color) * alpha,
+		mxw_get_color_g(base_color) * (1 - alpha)
+			+ mxw_get_color_g(fore_color) * alpha,
+		mxw_get_color_b(base_color) * (1 - alpha)
+			+ mxw_get_color_b(fore_color) * alpha));
 }
