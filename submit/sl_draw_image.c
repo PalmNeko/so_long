93a6000e873931cd9f510f8eb18fc35a17135313
PalmNeko <nitunamoni13579@gmail.com>
2024-06-15 23:10:37 +0900

    update: fix counter_header

--- submit/sl_draw_image.c
+++ submit/sl_draw_image.c
@@ -6,7 +6,7 @@
 /*   By: tookuyam <tookuyam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 17:19:58 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/06/15 22:14:42 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/06/15 22:52:37 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void sl_draw_image(t_sl_this *sl)
 		mxw_get_image_from_flipbook(*sl->player->now_flipbook),
 		sl->player->now_x - sl->camera_point.x,
 		sl->player->now_y - sl->camera_point.y);
-	mxw_put_image_to_window(sl->so_long_window, sl->counter_header, - sl->camera_point.x, - sl->camera_point.y);
+	mxw_put_image_to_window(sl->so_long_window, sl->counter_header, 0, 0);
 }
 
 void	sl_draw_map(t_sl_this *sl)
