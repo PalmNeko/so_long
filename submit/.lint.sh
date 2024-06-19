1cdc7df4174e7b700a3298be32b0c87d8a6a53da
PalmNeko <nitunamoni13579@gmail.com>
2024-06-19 16:25:21 +0900

    update: norm error format

--- submit/.lint.sh
+++ submit/.lint.sh
@@ -1,10 +1,23 @@
 
+norm_format () {
+    NORM_OUTPUT="$1"
+    FILE_NAME=$(echo "$NORM_OUTPUT" | head -n 1 | awk -F ':' '{print $1}')
+    ERROR_LOG=$(echo "$NORM_OUTPUT" | tail -n +2)
+    echo "$ERROR_LOG" | while read LINE; do
+        ROWNO=$(echo "$LINE" | grep -oE '[[:digit:]]+' | sed -n 1p)
+        COLNO=$(echo "$LINE" | grep -oE '[[:digit:]]+' | sed -n 2p)
+        ERR_TYPE=$(echo "$LINE" | awk '{print $2}')
+        ERR_MSG=$(echo "$LINE" | grep -oE '[[:alpha:]]([[:alpha:]]|[[:space:]])+$')
+        echo "Error: $FILE_NAME:$ROWNO:$COLNO $ERR_TYPE: $ERR_MSG"
+    done
+}
+
 FILES=$(find . \( -name "*.h" -o -name "*.c" \) -not -path "*libft*" -not -path "*minilibx*" )
 
 for file in ${FILES}; do
     RESULT="$(norminette $file | grep -v "OK")";
     if [ -n "$RESULT" ] ; then
-        echo "$RESULT"
+        norm_format "$RESULT"
         break 
     fi
 done
