19dd91391321fba1e89142d863bbafe6407d9a03
tookuyam <tookuyam@student.42tokyo.jp>
2024-06-21 00:09:30 +0900

    add: switch environment

--- submit/.test/wrap_main.c
+++ submit/.test/wrap_main.c
@@ -1,10 +1,22 @@
 
+#include <stdlib.h>
 #include <stdio.h>
+#include "libft.h"
+#include "cident.h"
 
 int	__real_main(int argc, char *argv[]);
 
 int	__wrap_main(int argc, char *argv[])
 {
-	printf("Hello wrap\n");
+	char	*error_count_down_str;
+	int		error_count_down;
+
+	if (getenv("IS_DEBUG") != NULL)
+		cident_set_debug(true);
+	error_count_down = 0;
+	error_count_down_str = getenv("ERROR_COUNT_DOWN");
+	error_count_down = ft_atoi(error_count_down_str);
+	if (error_count_down > 0)
+		cident_set_error(error_count_down);
 	return (__real_main(argc, argv));
 }
