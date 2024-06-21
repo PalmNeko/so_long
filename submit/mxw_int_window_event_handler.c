2c2bad7065863ba6a3ba8befee41d146910fc2a7
tookuyam <tookuyam@student.42tokyo.jp>
2024-06-21 20:54:41 +0900

    delete: mxw_resize_function

--- submit/mxw_int_window_event_handler.c
+++ submit/mxw_int_window_event_handler.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 21:47:11 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/06/21 18:55:46 by marvin           ###   ########.fr       */
+/*   Updated: 2024/06/21 20:54:00 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,21 +42,5 @@ int	mxw_int_destroy_handler(t_mxw_window *param)
 	}
 	param->has_mlx_win = false;
 	mxw_destroy_window(param);
-	return (0);
-}
-
-#include
-int mxw_int_resize_hander(t_mxw_window *param)
-{
-	int					index;
-	t_mxw_event_handler	*event;
-
-	index = 0;
-	while (index < param->event_handlers_cnt[ResizeRequest])
-	{
-		event = &param->event_handlers[ResizeRequest][index];
-		event->handler(event->param);
-		index++;
-	}
 	return (0);
 }
